6caa59bd78ebaf29eefc222b95b8ec50ea0b0c64
Martin Honis <martin.honis@capgemini.com>
2016-11-28 16:13:20 +0100

    add Mario states and animations

--- core/src/com/mhonis/mariobros/sprites/Mario.java
+++ core/src/com/mhonis/mariobros/sprites/Mario.java
@@ -1,12 +1,16 @@
 package com.mhonis.mariobros.sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.mhonis.mariobros.MarioBros;
 import com.mhonis.mariobros.screens.PlayScreen;
 
@@ -16,22 +20,58 @@ import com.mhonis.mariobros.screens.PlayScreen;
 
 public class Mario extends Sprite {
 
+    public enum State {FALLING, JUMPING, STANDING, RUNNING}
+
+    public State currentState;
+    public State previousState;
+
     public World world;
     public Body b2body;
 
     private TextureRegion marioStand;
+    private Animation marioRun;
+    private Animation marioJump;
+    private float stateTimer;
+    private boolean runningRight;
 
     public Mario(World world, PlayScreen screen) {
         super(screen.getTextureAtlas().findRegion("little_mario"));
         this.world = world;
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for (int i = 1; i < 4; i++)
+            frames.add(new TextureRegion(getTexture(), 1 + i * 16, 11, 16, 16)); //needs to be offset by the fireball texture
+
+        marioRun = new Animation(0.1f, frames);
+        frames.clear();
+        for (int i = 4; i < 6; i++)
+            frames.add(new TextureRegion(getTexture(), 1 + i * 16, 11, 16, 16)); //needs to be offset by the fireball texture
+        marioJump = new Animation(0.1f, frames);
+        frames.clear();
+
         marioStand = new TextureRegion(getTexture(), 1, 11, 16, 16);
-        setBounds(0, 0, 16/MarioBros.PPM, 16/MarioBros.PPM);
+        setBounds(0, 0, 16 / MarioBros.PPM, 16 / MarioBros.PPM);
         setRegion(marioStand);
         defineMario();
     }
 
+    public State getState() {
+        if (b2body.getLinearVelocity().y > 0 || (b2body.getLinearVelocity().y < 0 && previousState == State.JUMPING))
+            return State.JUMPING;
+        else if (b2body.getLinearVelocity().y < 0)
+            return State.FALLING;
+        else if (b2body.getLinearVelocity().x != 0)
+            return State.RUNNING;
+        return State.STANDING;
+    }
+
     public void update(float dt) {
-        setPosition(b2body.getPosition().x - getWidth()/2, b2body.getPosition().y - getHeight()/2);
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(dt));
     }
 
     public void defineMario() {
@@ -40,10 +80,50 @@ public class Mario extends Sprite {
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
+        //Problem with sphere is it bumps when traversing border between two tiles, EdgeShape seems to be the way to go
+        //further info: Ghost Vertices http://www.iforce2d.net/b2dtut/ghost-vertices
+
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
         shape.setRadius(6 / MarioBros.PPM);
         fdef.shape = shape;
+//        FixtureDef fdef = new FixtureDef();
+//        EdgeShape feet = new EdgeShape();
+//        feet.set(new Vector2(-2 / MarioBros.PPM, -8 / MarioBros.PPM), new Vector2(2 / MarioBros.PPM, -8 / MarioBros.PPM));
+//        fdef.shape = feet;
+//        fdef.isSensor = false;
         b2body.createFixture(fdef);
+    }
+
+    public TextureRegion getFrame(float dt) {
+        currentState = getState();
+
+        TextureRegion region;
+        switch (currentState) {
+            case JUMPING:
+                region = marioJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = marioRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+            default:
+                region = marioStand;
+                break;
+        }
+
+        if ((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = false;
+        } else if ((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = true;
+        }
+
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+
+        return region;
     }
 }
