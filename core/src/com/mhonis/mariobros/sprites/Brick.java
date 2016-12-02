fdcd7a9b20abb85f11cecbbef184e0265c719988
Martin Honis <martin.honis@capgemini.com>
2016-12-02 16:53:00 +0100

    add entity cathegory filters; implement brick break on head hit mechanic

--- core/src/com/mhonis/mariobros/sprites/Brick.java
+++ core/src/com/mhonis/mariobros/sprites/Brick.java
@@ -3,6 +3,7 @@ package com.mhonis.mariobros.sprites;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mhonis.mariobros.MarioBros;
 
 /**
  * Created by mhonis on 4.11.2016.
@@ -13,10 +14,13 @@ public class Brick extends InteractiveTileObject {
     public Brick(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
         fixture.setUserData(this);
+        setCathegoryFilter(MarioBros.BRICK_BIT);
     }
 
     @Override
     public void onHeadHit() {
         System.out.println("head hit a brick");
+        setCathegoryFilter(MarioBros.DESTROYED_BIT);
+        getCell().setTile(null);
     }
 }
--- core/src/com/mhonis/mariobros/sprites/Coin.java
+++ core/src/com/mhonis/mariobros/sprites/Coin.java
@@ -3,6 +3,7 @@ package com.mhonis.mariobros.sprites;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mhonis.mariobros.MarioBros;
 
 /**
  * Created by mhonis on 4.11.2016.
@@ -13,6 +14,7 @@ public class Coin extends InteractiveTileObject {
     public Coin(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
         fixture.setUserData(this);
+        setCathegoryFilter(MarioBros.COIN_BIT);
     }
 
     @Override
